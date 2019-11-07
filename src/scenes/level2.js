216f6f3080da4c2b29e9164901047179c7a779e4
greyhatchet <40676385+greyhatchet@users.noreply.github.com>
2019-11-07 14:32:21 -0600

    melee sound

--- src/scenes/level2.js
+++ src/scenes/level2.js
@@ -14,19 +14,14 @@ export default class level2 extends Phaser.Scene {
     console.log('[preload]')
     this.load.json("levelSetting","./src/data/levelSetting.json");
     this.load.image('background1', './assets/images/egyptianbackground.jpg');
-    this.load.image('bubble', './assets/images/opaquebubble.png');
-    this.load.image('spacebtn', './assets/images/spacebutton.png');
-  }
+}
 
   create() {
     console.log('[create]');
     // background
     this.add.image(2240,384,'background1');
 
-    //tutorial
-    this.add.image(180,330, 'bubble').setScale(.4,.4);
-    this.add.image(180,330, 'spacebtn').setScale(.3,.3);
-    this.add.text(155, 280, "Melee");
+
 
     //Add change scene event listeners
     ChangeScene.addSceneEventListeners(this);
@@ -35,6 +30,7 @@ export default class level2 extends Phaser.Scene {
     this.backgroundMusic = this.sound.add("creepy");
     this.backgroundMusic.play({loop:true});
     this.shootBeam = this.sound.add("beam");
+    this.meleeSound = this.sound.add("meleeAttack");
     this.yell = this.sound.add("diedYell");
     this.cry = this.sound.add("diedCry");
     this.pickupSound = this.sound.add("pickupSound");
@@ -54,11 +50,11 @@ export default class level2 extends Phaser.Scene {
     const worldTileset = map.addTilesetImage("inca_front", "incaFrontTiles");
 
     //render map/player/enemies in specific order
-    const bgLayer = map.createStaticLayer("Below Player", below2Tileset, 0, 0);
+    const bgLayer = map.createStaticLayer("Below Player", worldTileset, 0, 0);
     //const belowLayer = map.createStaticLayer("Below Player", belowTileset, 0, 0);
     const worldLayer = map.createStaticLayer("World", worldTileset, 0, 0);
     worldLayer.setCollisionByProperty({ collides: true });
-    worldLayer.setTileIndexCallback﻿﻿([31,32], this.hitExit, this);
+    worldLayer.setTileIndexCallback﻿﻿([30,28], this.hitExit, this);
 
     //diamonds
     this.collectItems = this.add.group();
@@ -125,8 +121,6 @@ export default class level2 extends Phaser.Scene {
       x: this.spawnX,
       y: this.spawnY
     });
-
-    const aboveLayer = map.createStaticLayer("Above Player", worldTileset, 0, 0);
 
     console.log('created map layers and sprites');
 
@@ -371,9 +365,6 @@ export default class level2 extends Phaser.Scene {
     if (player.isAttacking == false) {
       console.log('player was not attacking');
 
-      //enemy briefly disabled
-      enemy.stun();
-
       //variables to adjust player x away from enemy
       var enemyHalfWidth = enemy.width / 2;
       var enemyRightX = enemy.x + enemyHalfWidth;
@@ -387,7 +378,7 @@ export default class level2 extends Phaser.Scene {
       if (this.player.body.touching.down) {
         //collision on top or bottom of enemy
         enemyDied = true;
-
+        enemy.isActive = false;
         this.player.body.setVelocityY(-330);
 
       } else if (this.player.body.touching.right) {
@@ -395,16 +386,22 @@ export default class level2 extends Phaser.Scene {
         this.player.x = enemyLeftX - this.player.width;
         this.player.y = enemyBottomY - playerHalfHeight;
 
+        //enemy briefly disabled
+        enemy.stun();
+
         //player takes damage
-        player.updateHealth(75);  //75 ARBITRARILY CHOSEN
+        player.updateHealth(25);  //75 ARBITRARILY CHOSEN
 
       } else if (this.player.body.touching.left) {
         //collision on right side of enemy
         this.player.x = enemyRightX + this.player.width;
         this.player.y = enemyBottomY - playerHalfHeight;
 
+        //enemy briefly disabled
+        enemy.stun();
+
         //player takes damage
-        player.updateHealth(75);  //75 ARBITRARILY CHOSEN
+        player.updateHealth(25);  //25 ARBITRARILY CHOSEN
       }
 
       console.log("adjusted player coordinates: (" + player.x + ", " + player.y + ")");
@@ -412,7 +409,6 @@ export default class level2 extends Phaser.Scene {
     //HANDLE COLLISION IF PLAYER IS ATTACKING
     } else {
       console.log('player was attacking');
-
       //enemy dies
       enemyDied = true;
     }
