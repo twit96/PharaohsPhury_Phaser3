c692f37932371d41fde28c2ad61c7b42403ac73b
greyhatchet <40676385+greyhatchet@users.noreply.github.com>
2019-11-11 13:03:18 -0600

    Arrow trap first version

--- src/scenes/level1.js
+++ src/scenes/level1.js
@@ -87,6 +87,11 @@ export default class level1 extends Phaser.Scene {
     this.enemiesS = this.add.group();
     this.enemiesS.enableBody = true;
 
+    //arrow trap
+    this.arrow = this.physics.add.sprite(500, 800, "arrow");
+    this.arrow.body.setAllowGravity(false)
+    this.arrow.body.setVelocityY(-100)
+
     //CREATE LEVEL
     // level Data parse from json, read cordination into array of [x,y];
     this.levelSettingInfo = this.cache.json.get('levelSetting');
@@ -195,6 +200,7 @@ export default class level1 extends Phaser.Scene {
 
 
 
+
     this.physics.add.overlap(
       this.player,
       this.enemiesA,
@@ -213,6 +219,20 @@ export default class level1 extends Phaser.Scene {
       this.player.beams,
       worldLayer,
       this.player.beamHitWall,
+      null,
+      this
+    );
+    this.physics.add.overlap(
+      this.arrow,
+      worldLayer,
+      this.arrowHitWall,
+      null,
+      this
+    );
+    this.physics.add.overlap(
+      this.arrow,
+      this.player,
+      this.arrowHitPlayer,
       null,
       this
     );
@@ -567,4 +587,31 @@ export default class level1 extends Phaser.Scene {
     this.healthBarFill.setCrop(0,0,this.healthBarOrgWidth*this.player.health /100,this.healthBarOrgHeight);
   }
 
+  arrowHitWall(bullet, worldLayer, invisLayer) {
+    /*
+    function to check each worldLayer tile the soldier bullet overlaps with for
+    its collides property. destroys the bullet if it encounters a tile with
+    collides = true (i.e. the bullet hit a wall tile)
+    */
+    if (worldLayer.collides) {
+      console.log('[arrowHitWall]');
+      bullet.disableBody(true, true);
+    }
+
+  }
+
+  arrowHitPlayer(bullet, player) {
+    /*
+    function to handle overlap between player and tank shell
+    (i.e. tank shell hit player)
+    */
+    console.log('[arrowHitPlayer]');
+    //this.bomb.play();
+
+    //disable shell
+    bullet.disableBody(true, true);
+
+    //update player stats
+    this.player.updateHealth(50);
+  }
 }
