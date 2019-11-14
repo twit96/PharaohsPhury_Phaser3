80bc7bbce79b4333f7195b5eadb27f225b5d8a78
Tyler <twit96@users.noreply.github.com>
2019-11-13 23:00:37 -0600

    removed index.js calls to old levels

--- src/index.js
+++ src/index.js
@@ -7,15 +7,6 @@ import levelPicker from './scenes/levelPicker.js';
 
 import levelScene from './scenes/level.js';
 
-import level1 from './scenes/level1.js';
-import level2 from './scenes/level2.js';
-import level3 from './scenes/level3.js';
-import level4 from './scenes/level4.js';
-import level5 from './scenes/level5.js';
-import level6 from './scenes/level6.js';
-import level7 from './scenes/level7.js';
-import level8 from './scenes/level8.js';
-
 import finalBossLevel from './scenes/finalBossLevel.js';
 import gameOverScene from './scenes/gameOverScene.js';
 
@@ -26,17 +17,7 @@ class Game extends Phaser.Game {
     this.scene.add('menu', menu);
     this.scene.add('userScene', userScene);
     this.scene.add('levelPicker', levelPicker);
-
     this.scene.add('levelScene', levelScene);
-
-    this.scene.add('level1', level1);
-    this.scene.add('level2', level2);
-    this.scene.add('level3', level3);
-    this.scene.add('level4', level4);
-    this.scene.add('level5', level5);
-    this.scene.add('level6', level6);
-    this.scene.add('level7', level7);
-    this.scene.add('level8', level8);
     this.scene.add('finalBossLevel', finalBossLevel);
     this.scene.add('gameOverScene', gameOverScene);
     this.scene.start('Boot');
