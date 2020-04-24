ffbbcd6db701484e31c4b233935a2ee881b9bdf6
Tyler <twit96@users.noreply.github.com>
2020-04-23 22:36:51 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 /*global Phaser, window*/
-import Config from './config/config.js';
-import BootScene from './scenes/BootScene.js';
-import menu from './scenes/menu.js';
-import userScene from './scenes/userRegister.js';
-import levelPicker from './scenes/levelPicker.js';
-import levelScene from './scenes/level.js';
-import gameOverScene from './scenes/gameOverScene.js';
+import Config from './src/config/config.js';
+import BootScene from 'scenes/BootScene.js';
+import menu from 'scenes/menu.js';
+import userScene from 'scenes/userRegister.js';
+import levelPicker from 'scenes/levelPicker.js';
+import levelScene from 'scenes/level.js';
+import gameOverScene from 'scenes/gameOverScene.js';
 
 class Game extends Phaser.Game {
   constructor () {
